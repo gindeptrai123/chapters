4487f636c39e602610588fed7311bf0bfd90eb1a
Your Name <tranquanghuy1280@gmail.com>
2019-09-30 21:57:47 +0700

    chapter6 edit sau khi merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   before_save{email.downcase!}
-  validates :name, presence: true, length: {maximum: Settings.validates.name}
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.validates.email},
+  validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true,
-    length: {minimum: Settings.validates.password}
+  validates :password, presence: true, length: {minimum: 6}
   has_secure_password
 end
